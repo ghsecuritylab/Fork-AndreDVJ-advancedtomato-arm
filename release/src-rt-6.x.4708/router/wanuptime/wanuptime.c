87186fd4520ba3034f7e7557bcdc8e19ed3c36fe
AndreDVJ <andre@debian>
2016-03-12 17:55:32 -0300

    Fixed segmentation fault - Thanks koitsu for the insights.

--- release/src-rt-6.x.4708/router/wanuptime/wanuptime.c
+++ release/src-rt-6.x.4708/router/wanuptime/wanuptime.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-#include <sys/sysinfo.h>
 #include <time.h>
+#include <sys/sysinfo.h>
 
 int main(int argc, char **argv)
 {
-  struct sysinfo si;
-  time_t uptime;
+    struct sysinfo si;
+    time_t uptime;
 
-  sysinfo(&si);
-  if (check_wanup() && (f_read("/var/lib/misc/wantime", &uptime, sizeof(time_t)) ==  sizeof(uptime))) {
-    printf("%ld\n",si.uptime - uptime);
-  }
-  else
-    printf("0\n");
-  return 0;
+    if (sysinfo(&si) == -1) {
+        return 1;
+    }
+    if (f_read("/var/lib/misc/wan_time", &uptime, sizeof(time_t)) == sizeof(uptime)) {
+        printf("%ld\n", si.uptime - uptime);
+    }
+    else {
+        printf("0\n");
+    }
+
+    return 0;
 }
