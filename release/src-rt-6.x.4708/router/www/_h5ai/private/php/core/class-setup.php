64b240f68a23dfcea7cd5dee7097daa0806b7dbc
oneleft <oneleft.ak@gmail.com>
2016-04-22 19:31:41 -0800

    h5ai: Modified session ROOT_PATH variable to be set by fastcgi_param ROOT_PATH instead _h5ai absolute path

--- release/src-rt-6.x.4708/router/www/_h5ai/private/php/core/class-setup.php
+++ release/src-rt-6.x.4708/router/www/_h5ai/private/php/core/class-setup.php
@@ -106,7 +106,7 @@ class Setup {
         $this->set('H5AI_PATH', Util::normalize_path(dirname(dirname(dirname(dirname(__FILE__)))), false));
 
         $this->set('ROOT_HREF', Util::normalize_path(dirname($this->get('H5AI_HREF')), true));
-        $this->set('ROOT_PATH', Util::normalize_path(dirname($this->get('H5AI_PATH')), false));
+        $this->set('ROOT_PATH', getenv('ROOT_PATH'));
 
         $this->set('PUBLIC_HREF', Util::normalize_path($this->get('H5AI_HREF') . '/public/', true));
         $this->set('PUBLIC_PATH', Util::normalize_path($this->get('H5AI_PATH') . '/public/', false));
