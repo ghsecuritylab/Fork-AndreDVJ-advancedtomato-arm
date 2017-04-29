Jackysi/advancedtomato-arm

R8000: fix wireless performance and stability
Also this commit temporary added WPS led as WAN led, because i didn`t find solution to activate WAN led.

release/src-rt-6.x.4708/router/shared/misc.c
@@ -292,6 +292,9 @@ int check_wanup(char *prefix)
 		if (get_model() == MODEL_R6300v2) {
 			led(LED_WHITE,LED_OFF);
 		}
+		if (get_model() == MODEL_R8000) {
+			led(LED_WHITE,LED_OFF);
+		}
 		return 0;
 	}
 
@@ -358,6 +361,9 @@ int check_wanup(char *prefix)
 		led(LED_WHITE,up);
 	}
 	if (get_model() == MODEL_R6300v2) {
+		led(LED_WHITE,up);
+	}
+	if (get_model() == MODEL_R8000) {
 		led(LED_WHITE,up);
 	}
 

ghsecuritylab/Fork-AndreDVJ-advancedtomato-arm

Conflicts:
	release/src-rt-6.x.4708/router/shared/misc.c

release/src-rt-6.x.4708/router/shared/misc.c
@@ -289,6 +289,9 @@ int wan_led(int *mode) // mode: 0 - OFF, 1 - ON
 	if (model == MODEL_R6300v2) {
 		led(LED_WHITE,mode);
 	}
+	if (model == MODEL_R8000) {
+		led(LED_WHITE,mode);
+	}
 
 	return mode;
 }