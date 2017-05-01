85e36070de7dbf5ad6ae9a940073e174277b41c6
AndreDVJ <andre@debian>
2017-05-01 14:44:08 -0300

    Turn on/off WiFi status LED according to the overall radio statuses
    Genie turns that LED ON if Wifi is enabled, regardless of which of the radios are enabled, so trying to match behavior of Netgear OFW

--- release/src-rt-6.x.4708/router/rc/network.c
+++ release/src-rt-6.x.4708/router/rc/network.c
@@ -591,6 +591,10 @@ void restart_wl(void)
 			led(LED_WLAN, LED_OFF);
 		if (nvram_match("wl1_radio", "0"))
 			led(LED_5G, LED_OFF);
+		if (nvram_match("wl0_radio", "1") || nvram_match("wl1_radio", "1"))
+			led(LED_AOSS, LED_ON);
+		else
+			led(LED_AOSS, LED_OFF);
 	}
 
 	if (get_model() == MODEL_WS880) {
@@ -708,7 +712,7 @@ void start_wl(void)
 	if (is_client)
 		xstart("radio", "join");
 	
-	if ((get_model() == MODEL_R6400) || get_model() == MODEL_R7000) {
+	if ((get_model() == MODEL_R6400) || (get_model() == MODEL_R7000)) {
 		if (nvram_match("wl0_radio", "1"))
 			led(LED_WLAN, LED_ON);
 		if (nvram_match("wl1_radio", "1"))
@@ -717,6 +721,10 @@ void start_wl(void)
 			led(LED_WLAN, LED_OFF);
 		if (nvram_match("wl1_radio", "0"))
 			led(LED_5G, LED_OFF);
+		if (nvram_match("wl0_radio", "1") || nvram_match("wl1_radio", "1"))
+			led(LED_AOSS, LED_ON);
+		else
+			led(LED_AOSS, LED_OFF);
 	}
 
 	if (get_model() == MODEL_WS880) {
